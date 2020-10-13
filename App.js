d1e9440c7c8f15896a0785a221503faf8a2fd77b
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-13 21:02:30 +0100

    understanding how interpolate works

--- App.js
+++ App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import { Animated, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-const position = new Animated.ValueXY({x:0, y:0})
-Animated.timing(position,{useNativeDriver: true, toValue:{x:200, y:500}, duration:3000}).start()
+const position = new Animated.ValueXY({x:-100, y:-200})
+Animated.timing(position,{useNativeDriver: true, toValue:{x:175, y:250}, duration:4000}).start()
 
+const rotate = position.x.interpolate({
+  inputRange:[0,200],
+  outputRange:["0deg", "360deg"]
+})
   return (
     <View style={styles.container}>
       <Animated.View style={{height:80, width: 80, alignItems: 'center',
         justifyContent: 'center',backgroundColor: 'red',transform:[
           {translateX:position.x},
-          {translateY:position.y}
+          {translateY:position.y},
+          {rotate:rotate}
         ]}}>
 
       <Text>cnq</Text>
