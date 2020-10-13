cfbbe80585bc4fd94261739cd3a01fa91fc160f6
michaldobiezynski <michaldobiezynski@hotmail.com>
2020-10-13 21:14:41 +0100

    handling gestures

--- App.js
+++ App.js
@@ -1,17 +1,38 @@
 import React from 'react';
-import { Animated, StyleSheet, Text, View } from 'react-native';
+import { Animated, PanResponder, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
-const position = new Animated.ValueXY({x:-100, y:-200})
-Animated.timing(position,{useNativeDriver: true, toValue:{x:175, y:250}, duration:4000}).start()
+const position = new Animated.ValueXY({x:0, y:0})
+// Animated.timing(position,{useNativeDriver: true, toValue:{x:175, y:250}, duration:4000}).start()
+
+const pan = PanResponder.create({
+  onMoveShouldSetPanResponder: () => true,
+  onPanResponderMove: (e, gesture) => {
+    position.setValue({x:gesture.dx, y:gesture.dy})
+  },
+  
+  // Animated.event([
+  //   null, {dx:position.x,dy:position.y}
+  // ]),
+
+  onPanResponderRelease: () => {
+    // position.setValue({x:0, y:0})
+    Animated.spring(position,{
+      toValue:{x:0, y:0}
+    }).start()
+  }
+
+})
 
 const rotate = position.x.interpolate({
-  inputRange:[0,200],
-  outputRange:["0deg", "360deg"]
+  inputRange:[0,50],
+  outputRange:["0deg", "90deg"]
 })
   return (
     <View style={styles.container}>
-      <Animated.View style={{height:80, width: 80, alignItems: 'center',
+      <Animated.View 
+      {...pan.panHandlers}
+      style={{height:80, width: 80, alignItems: 'center',
         justifyContent: 'center',backgroundColor: 'red',transform:[
           {translateX:position.x},
           {translateY:position.y},
